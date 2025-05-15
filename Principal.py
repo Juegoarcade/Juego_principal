d0a3a647c8985928b99d6e428dcb9289dcca8a78
LAMALAMOSO <otras_14_cosas@outlook.com>
2025-05-15 19:13:26 +0200

    Update Principal.py

--- Principal.py
+++ Principal.py
@@ -2,15 +2,25 @@
 from pygame import*
 from random import randint
 
+
 bala_enemigo = "bl.png"
 bala_disparo = "bl2.png"
 bowser = "bw.jpg"
 llave = "ll.png"
 mario_arma = "mrp.png"
 mario ="mr.jpg"
-princesa = "p.jpg"
+princesa = "pp.jpg"
 estrella = "str.png"
 fondo_general = "fn.jpg"
+fondo_victoria = "fv.jpeg"
+fondo_derrota = "fd.jpeg"
+
 
 win_width = 1000
 win_height = 700
+
+
+run = true
+
+while run == true:
+  if 

2db4b7c10d7221c35d216cfd907f679937d71920
Reki-b <ikerbjpokemon@gmail.com>
2025-05-15 18:44:11 +0200

    Update Adri.py

--- Adri.py
+++ Adri.py
@@ -1,1 +1,15 @@
+#Funciones importadas
+from pygame import*
+from random import randint
 
+bala_enemigo = "bl.png"
+bala_disparo = "bl2.png"
+bowser = "bw.jpg"
+llave = "ll.png"
+mario_arma = "mrp.png"
+mario ="mr.jpg"
+princesa = "p.jpg"
+estrella = "str.png"
+
+win_width = 1000
+win_height = 700

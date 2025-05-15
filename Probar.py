837e930ab12f8743f007640b1e194a9e61594b3e
Dani <daniel.moreno.bazan@gmail.com>
2025-05-15 18:44:06 +0200

    Update Probar.py

--- Probar.py
+++ Probar.py
@@ -2,6 +2,18 @@
 from pygame import*
 from random import randint
 
+bala_enemigo = "bl.png"
+bala_disparo = "bl2.png"
+bowser = "bw.jpg"
+llave = "ll.png"
+mario_arma = "mrp.png"
+mario ="mr.jpg"
+princesa = "p.jpg"
+estrella = "str.png"
+
+win_width = 1000
+win_height = 700
+
 #Pantalla
 win_height = 700
 win_width = 1200

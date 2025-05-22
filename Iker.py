b538127f013c4dbf2d52f07fdfb996aaaacb4209
Reki-b <ikerbjpokemon@gmail.com>
2025-05-22 19:15:45 +0200

    Update Iker.py

--- Iker.py
+++ Iker.py
@@ -1,41 +1,22 @@
 #Iker
-win_width = 800
-
-# Shift de fondo y velocidad actual
-shift = 0
-speed = 0
-
-# Límites de movimiento del personaje (cuando el fondo empieza a moverse)
-left_bound = win_width / 40
-right_bound = win_width - 8 * left_bound
-
-
-2. Evento de teclado (en tu loop principal, dentro del ciclo for event in pygame.event.get():)
-
-
-    
-
-
-3. Actualizar movimiento del fondo y del personaje (en el bucle principal del juego, después de procesar los eventos)
-if (robin.rect.x > right_bound and robin.x_speed > 0) or (robin.rect.x < left_bound and robin.x_speed < 0):
-    shift -= robin.x_speed  # mover el fondo en sentido opuesto
-    for s in all_sprites:  # mover todos los objetos
-        s.rect.x -= robin.x_speed
-else:
-    robin.rect.x += robin.x_speed  # si no cruzó los límites, el personaje se mueve normalmente
-
-
-    4. Actualizar fondo (scroll infinito)
-local_shift = shift % win_width
-
-window.blit(background_picture, (local_shift, 0))
-if local_shift != 0:
-    window.blit(background_picture, (local_shift - win_width, 0))
-
-5. Dibujar sprites y actualizar pantalla (al final del ciclo principal)
-
-# Dibujar todos los objetos
-all_sprites.draw(window)
-
-# Actualizar la pantalla
-pygame.display.update()
+(500, 530, 200, 20),  # bajo1
+(750, 530, 150, 20),  # bajo2
+(950, 530, 150, 20),  # bajo3
+(300, 380, 150, 20),  # medio1
+(500, 380, 150, 20),  # medio2
+(700, 380, 150, 20),  # medio3
+(900, 380, 150, 20),  # medio4
+(1100, 380, 150, 20), # medio5
+(1100, 480, 150, 20), # bajo4
+(1500, 380, 150, 20), # medio6
+(1700, 530, 150, 20), # bajo5
+(1900, 480, 200, 20), # bajo6
+(2100, 430, 100, 20), # bajo7
+(2300, 580, 200, 20), # bajo8 (más cercano al suelo)
+(2500, 480, 200, 20), # bajo9
+(2700, 380, 100, 20), # medio7
+(1000, 280, 20, 100), # muro1
+(1300, 280, 20, 100), # muro2
+(1600, 280, 20, 100), # muro3
+(1900, 280, 20, 100), # muro4
+(2200, 280, 20, 100), # muro5
